fcd474c2b66eef4a3fcbe5c4d112c5d7fab59c50
tamerharsh <htharshu@gmail.com>
2020-04-10 20:06:53 +0530

    [#4]  DS for the lib .

---
+++ logger/include/logger_lib.hpp
@@ -0,0 +1,25 @@
+#ifndef LOGGER_LIB_HPP
+#define LOGGER_LIB_HPP
+
+#include <string>
+#include <memory>
+
+namespace logger
+{
+	/**
+	* @brief  Log level with their severity order , 0 =>high priority , 5=> least priority. 
+	*/
+	enum class LEVEL = { OFF = 0, FATAL = 1, ERROR = 2,WARN = 3,DEBUG = 4,TRACE = 5 };
+
+	/**
+	* @brief  Interface for the extracted log data like file name , function name etc , one may extend for more parameters such as number of argumnets __VA_ARGS__.
+	*/
+	struct IExtarctedLogCredentials
+	{
+		std::string file_name = "";
+		std::string function_name = "";
+		int line_number = 0;
+	};
+
+};
+#endif()
