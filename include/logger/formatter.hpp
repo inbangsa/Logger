3b483a8bf911e0034e56d7e02e103afdcb80fb54
tamerharsh <htharshu@gmail.com>
2020-04-16 13:22:49 +0530

    [#5] Changed func names,ds in common.hpp,made interface func for virtual func.

--- include/logger/formatter.hpp
+++ include/logger/formatter.hpp
@@ -1,20 +1,9 @@
 #ifndef FORMATTER_HPP
 #define FORMATTER_HPP
 
-#include <string>
-#include <memory>
+#include "common.hpp"
 
 namespace logger {
-/**
- *@ brief  For specifying various severity levels.
- */
-extern enum class LEVEL;
-
-/**
- *@ brief  Data Structure for storing various extracted data  like function name , file name etc for a log .
- */
-extern struct ExtractedLogCredentials;
-
 /**
  * @brief  Interface for formatting the given data.
  */
@@ -26,15 +15,29 @@ public:
    * @param pattern formatting pattern
    * @retval std::string
    */
-  void set_format_pattern(std::string format_pattern);
+  void i_set_format_pattern(std::string format_pattern);
 
   /**
    * @brief Returns formatting pattern.
    * @param None
    * @retval std::string
    */
-  std::string get_format_pattern() const;
+  std::string i_get_format_pattern() const;
 
+   /**
+   * @brief Interface for accessing format function.
+   * @param msg log message
+   * @param level log level
+   * @param logger_name default name is root
+   * @param log_credentials extracted log credentials
+   * @retval std::string
+   */
+  std::string i_format_data(std::string msg,
+    logger::LEVEL level,
+    std::string &logger_name,
+    std::shared_ptr<logger::ExtractedLogCredentials> &log_credentials);
+
+private:
   /**
    * @brief Formats the given data.
    * @param msg log message
@@ -52,8 +55,16 @@ private:
   std::string format_pattern;
 };
 
-void logger::IFormatter::set_format_pattern(std::string format_pattern) { this->format_pattern = format_pattern; }
+void logger::IFormatter::i_set_format_pattern(std::string format_pattern) { this->format_pattern = format_pattern; }
 
-void logger::IFormatter::get_format_pattern() const { return format_pattern; }
+std::string logger::IFormatter::i_get_format_pattern() const { return format_pattern; }
+
+std::string logger::IFormatter::i_format_data(std::string msg,
+  logger::LEVEL level,
+  std::string &logger_name,
+  std::shared_ptr<logger::ExtractedLogCredentials> &log_credentials)
+{
+  return this->format(msg, level, logger_name, log_credentials);
+}
 };// namespace logger
 #endif()
--- include/logger/logger_lib.hpp
+++ include/logger/logger_lib.hpp
@@ -2,21 +2,8 @@
 #define LOGGER_LIB_HPP
 
 #include "formatter.hpp"
-namespace logger {
-/**
- * @brief  Log level with their severity order , 0 =>high priority , 5=> least priority.
- */
-enum class LEVEL : { OFF = 0, FATAL = 1, ERROR = 2, WARN = 3, DEBUG = 4, TRACE = 5 };
 
-/**
- * @brief  Interface for the extracted log data like file name , function name etc , one may extend for more parameters
- * such as number of argumnets __VA_ARGS__.
- */
-struct ExtractedLogCredentials
-{
-  std::string file_name = "";
-  std::string function_name = "";
-  int line_number = 0;
-};
+namespace logger {
+
 };// namespace logger
 #endif()
