366d94ca3d6f50e2ec6e11aa821a8032602499e4
tamerharsh <htharshu@gmail.com>
2020-06-11 17:42:20 +0530

    [#27] Demonstrated sample usage for lib.

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -4,7 +4,32 @@
 #include <iostream>
 #include "logger/logger_lib.hpp"
 
-bool Divide(int number_1, int number_2) 
+
+void CreateLoggerObject()
+{
+logger::Logger logging_obj ;
+logging_obj.Log(logger::log_level::DEBUG, "sample message");
+}
+
+void SetSomeFeaturesUsingObject() 
+{
+  auto logging_obj = logger::Logger();
+   logging_obj.SetLoggerName("new logger");
+   logging_obj.SetLogLevel(logger::log_level::WARN);
+   logging_obj.Log(logger::log_level::DEBUG, "sample message");
+}
+
+void LogDirectlyUsingMacro()
+{ logger::OFF("sample message"); }
+
+
+void SetSomeFeaturesUsingMacro() {
+logger::SET_LOG_LEVEL(logger::log_level::WARN);
+logger::SET_LOGGER_NAME("new logger");
+logger::LOG(logger::log_level::DEBUG, "sample message");
+}
+
+bool SampleDivideFunction(int number_1, int number_2) 
 { 
   if (number_2 == 0) {
     throw std::exception("Divisor cannot be zero.");
@@ -15,9 +40,13 @@ bool Divide(int number_1, int number_2)
 int main()
 {
   try {
-    Divide(10, 0);  
+    CreateLoggerObject();
+    SetSomeFeaturesUsingObject();
+    LogDirectlyUsingMacro();
+    SetSomeFeaturesUsingMacro();
+    SampleDivideFunction(10, 0);
   } catch (std::exception ex) {
-    logger::FATAL("Divisor is zero.");
+    logger::FATAL(ex.what());
       std::cout << ex.what();
   }
   return 0;
