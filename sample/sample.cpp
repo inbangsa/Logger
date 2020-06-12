84d69ed5f4569e19ea40fd7806f10b257ea67bc2
tamerharsh <htharshu@gmail.com>
2020-06-12 14:58:19 +0530

    [#27] Fixed indentation issue.

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -4,29 +4,27 @@
 #include <iostream>
 #include "logger/logger_lib.hpp"
 
-
 void CreateLoggerObject()
 {
-logger::Logger logging_obj ;
-logging_obj.Log(logger::log_level::DEBUG, "sample message");
+   logger::Logger logging_obj ;
+   logging_obj.Log(logger::log_level::DEBUG, "sample message");
 }
 
 void SetSomeFeaturesUsingObject() 
 {
-  auto logging_obj = logger::Logger();
+   auto logging_obj = logger::Logger();
    logging_obj.SetLoggerName("new logger");
    logging_obj.SetLogLevel(logger::log_level::WARN);
    logging_obj.Log(logger::log_level::DEBUG, "sample message");
 }
 
-void LogDirectlyUsingMacro()
-{ logger::OFF("sample message"); }
+void LogDirectlyUsingMacro(){logger::OFF("sample message");}
 
-
-void SetSomeFeaturesUsingMacro() {
-logger::SET_LOG_LEVEL(logger::log_level::WARN);
-logger::SET_LOGGER_NAME("new logger");
-logger::LOG(logger::log_level::DEBUG, "sample message");
+void SetSomeFeaturesUsingMacro()
+{
+  logger::SET_LOG_LEVEL(logger::log_level::WARN);
+  logger::SET_LOGGER_NAME("new logger");
+  logger::LOG(logger::log_level::DEBUG, "sample message");
 }
 
 bool SampleDivideFunction(int number_1, int number_2) 
@@ -47,7 +45,7 @@ int main()
     SampleDivideFunction(10, 0);
   } catch (std::exception ex) {
     logger::FATAL(ex.what());
-      std::cout << ex.what();
+    std::cout << ex.what();
   }
   return 0;
 }
